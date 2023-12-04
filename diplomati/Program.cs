1eeeb0a82d153e00ce91867741e748cc54a6ac23
alsakagy <alexdamato2006@libero.it>
2023-12-04 09:57:39 +0100

    finito

--- diplomati/Program.cs
+++ diplomati/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,155 +11,78 @@ namespace diplomati
         protected string _Nome;
         protected string _Cognome;
         protected int _Voto;
-
         public void Inserimento_Diplomati(string a, string b, int c)
         {
             _Nome = a;
             _Cognome = b;
             _Voto = c;
         }
-        public void Stampa_Diplomati(Diplomati[] a)
+
+        public void Stampa_Ammessi()
         {
-            if (a[0]._Voto >= 60)
-            {
-                Console.WriteLine("Studenti nuovi diplomati:");
-                foreach (Diplomati studente in a)
-                {
-                    Console.WriteLine($"Nome e cognome: {studente._Nome} {studente._Cognome} Voto: {studente._Voto}/100");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Studenti nuovi diplomati:");
-                foreach (Diplomati studente in a)
-                {
-                    Console.WriteLine($"Nome e cognome: {studente._Nome} {studente._Cognome} Voto: {studente._Voto}/59");
-                }
-            }
+
         }
-        public void Stampa_Ammessi(Diplomati[] a)
+        public void Stampa_Diplomati()
         {
-            if (a[0]._Voto >= 60)
-            {
-                Console.WriteLine("Diplomati nuovi ammessi hai concorsi:");
-                foreach (Diplomati studente in a)
-                {
-                    if (studente._Voto >= 70)
-                        Console.WriteLine($"Nome e cognome: {studente._Nome} {studente._Cognome} Voto: {studente._Voto}/100");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Diplomati vecchi ammessi hai concorsi:");
-                foreach (Diplomati studente in a)
-                {
-                    if (studente._Voto >= 42)
-                        Console.WriteLine($"Nome e cognome: {studente._Nome} {studente._Cognome} Voto: {studente._Voto}/60");
-                }
-            }
+
         }
     }
     class Nuovi_Diplomati : Diplomati
     {
-        //public Nuovi_Diplomati()
-        //{
-        //    _Nome = "";
-        //    _Cognome = "";
-        //    _Voto = 0;
-        //}
 
-        // Possibile aggiunta di funzioni uniche per nuovi (non è questo il caso)
     }
     class Vecchi_Diplomati : Diplomati
     {
-        //public Vecchi_Diplomati()
-        //{
-        //    _Nome = "";
-        //    _Cognome = "";
-        //    _Voto = 0;
-        //}
 
-        // Possibile aggiunta di funzioni uniche per vecchi (non è questo il caso)
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Nuovi_Diplomati[] Vettore_Nuovi = new Nuovi_Diplomati[100];
-            Vecchi_Diplomati[] Vettore_Vecchi = new Vecchi_Diplomati[100];
-
             bool Controllo = true;
+            int nuovi = 0, vecchi = 0;
+
+            Nuovi_Diplomati[] Vettore1 = new Nuovi_Diplomati[100];
+            Vecchi_Diplomati[] Vettore2 = new Vecchi_Diplomati[100];
+            Nuovi_Diplomati Studente1 = new Nuovi_Diplomati();
+            Vecchi_Diplomati Studente2 = new Vecchi_Diplomati();
 
             Console.WriteLine("quanti diplomati vuoi inserire");
             int quantità = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Inserisci il nome del diplomato");
-            string Nome = Console.ReadLine();
-            Console.WriteLine("Inserisci il Cognome del diplomato");
-            string Cognome = Console.ReadLine();
-            Console.WriteLine("Inserisci il voto del diplomato");
-            int Voto = int.Parse(Console.ReadLine());
-
-            // Controllo voto
-            while (Controllo)
-            {
-                if (Voto >= 36 && Voto <= 100)
-                {
-                    Controllo = false;
-                }
-                else
-                {
-                    Console.WriteLine("il voto inserito non è valido, inserisci un numero tra 36 e 100");
-                    Voto = int.Parse(Console.ReadLine());
-                }
-            }
-
-            // inserimento e divisione dei dati
             for (int i = 0; i < quantità; i++)
             {
-                if (Voto >= 61)
-                {
-                    if(Vettore_Nuovi != null)
-                        Vettore_Nuovi[i].Inserimento_Diplomati(Nome, Cognome, Voto);
-                }
-                else
-                {
-                    if (Vettore_Nuovi != null)
-                        Vettore_Vecchi[i].Inserimento_Diplomati(Nome, Cognome, Voto);
-                }
-            }
+                // inserimento dati
+                string Nome, Cognome;
+                int Voto;
+                Console.WriteLine("Inserisci il nome del diplomato");
+                Nome = Console.ReadLine();
+                Console.WriteLine("Inserisci il Cognome del diplomato");
+                Cognome = Console.ReadLine();
+                Console.WriteLine("Inserisci il voto del diplomato");
+                Voto = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("\n----------------------------------------------------------\n");
-
-            // Stampa dei diplomati
-            for (int i = 0; i < quantità; ++i)
-            {
-                if (Vettore_Nuovi[i] != null)
+                while (Controllo)
                 {
-                    if (Vettore_Nuovi != null)
-                        Vettore_Nuovi[i].Stampa_Diplomati(Vettore_Nuovi);
+                    if (Voto >= 61 && Voto <= 100)
+                    {
+                        Studente1.Inserimento_Diplomati(Nome, Cognome, Voto);
+                        Vettore1[nuovi] = Studente1;
+                        Controllo = false;
+                    }
+                    else if (Voto >= 36 && Voto <= 60)
+                    {
+                        Studente2.Inserimento_Diplomati(Nome, Cognome, Voto);
+                        Vettore2[vecchi] = Studente2;
+                        Controllo = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("il voto inserito non è un voto accettabile, rinserisci il voto del diplomato");
+                        Voto = int.Parse(Console.ReadLine());
+                    }
                 }
-                else if (Vettore_Vecchi[i] != null)
-                {
-                    if (Vettore_Nuovi != null)
-                        Vettore_Vecchi[i].Stampa_Diplomati(Vettore_Vecchi);
-                }
-            }
-
-            Console.WriteLine("\n----------------------------------------------------------\n");
-
-            // Spampa dei diplomati con accesso hai concorsi
-            for (int i = 0; i < quantità; ++i)
-            {
-                if (Vettore_Nuovi[i] != null)
-                {
-                    Vettore_Nuovi[i].Stampa_Ammessi(Vettore_Nuovi);
-                }
-                else if (Vettore_Vecchi[i] != null)
-                {
-                    Vettore_Vecchi[i].Stampa_Ammessi(Vettore_Vecchi);
-
-                }
+                Console.ReadLine();
             }
         }
     }
